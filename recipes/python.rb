d8050780297f0e08b6935c9fc29669c970515d22
Jeff Hutchison <jeff@jeffhutchison.com>
2016-02-27 13:40:39 -0500

    Resolve rubocop warnings.

--- recipes/python.rb
+++ recipes/python.rb
@@ -1,36 +1,34 @@
 
+include_recipe 'jhdc-web::build_essential'
 
-node['python']['dev_packages'].each do |pkg|
-  package pkg
-end
-
-configure_options = node['python']['configure_options'].join(" ")
+configure_options = node['python']['configure_options'].join(' ')
 
 version = node['python']['version']
 pip_version = node['python']['pip_version']
 
-install_path = "#{node['python']['prefix_dir']}/lib/python#{version.split(/(^\d+\.\d+)/)[1]}"
+install_path = "#{node['python']['prefix_dir']}" \
+               "/lib/python#{version.split(/(^\d+\.\d+)/)[1]}"
 
 remote_file "#{Chef::Config[:file_cache_path]}/Python-#{version}.tar.xz" do
   source "#{node['python']['url']}/#{version}/Python-#{version}.tar.xz"
   checksum node['python']['checksum']
-  mode "0644"
-  not_if { ::File.exists?(install_path) }
+  mode '0644'
+  not_if { ::File.exist?(install_path) }
 end
 
-bash "build-and-install-python" do
+bash 'build-and-install-python' do
   cwd Chef::Config[:file_cache_path]
   code <<-EOF
   tar -Jxvf Python-#{version}.tar.xz
   (cd Python-#{version} && ./configure #{configure_options})
   (cd Python-#{version} && make && make install)
   EOF
-  not_if { ::File.exists?(install_path) }
+  not_if { ::File.exist?(install_path) }
 end
 
-bash "upgrade-pip" do
+bash 'upgrade-pip' do
   code <<-EOF
   pip3 install --force --upgrade pip==#{pip_version}
   EOF
-  not_if "[[ `pip3 -V` =~ \"pip #{pip_version}\" ]]"
+  not_if "[[ `pip3 -V` =~ \'pip #{pip_version}\' ]]"
 end
