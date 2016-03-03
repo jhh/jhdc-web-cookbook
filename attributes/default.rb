e998b99c52b765d71c068cb0e32b49f799e47610
Jeff Hutchison <jeff@jeffhutchison.com>
2016-03-03 14:44:34 -0500

    Ruby build needs bzip2.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@ default['python']['MD5_checksum'] = 'e9ea6f2623fffcdd871b7b19113fde80'
 default['python']['prefix_dir'] = '/usr/local'
 default['python']['configure_options'] = %W(--prefix=#{python['prefix_dir']})
 
-default['build-essentials']['packages'] = %w(git zlib-devel openssl-devel
+default['build-essentials']['packages'] = %w(git bzip2 zlib-devel openssl-devel
                                              sqlite-devel bzip2-devel
                                              libffi-devel readline-devel
                                              ncurses-devel expat-devel
