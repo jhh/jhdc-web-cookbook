15a929e85d8d07669e0345757a27af543000dc59
Jeff Hutchison <jeff@jeffhutchison.com>
2016-03-06 14:44:04 -0500

    Update SSL config.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jeff@jeffhutchison.com'
 license          'All rights reserved'
 description      'Installs/Configures web services'
 long_description 'Installs/Configures web services'
-version          '1.6.3'
+version          '1.6.8'
 supports         'centos'
 
 depends 'build-essential'
